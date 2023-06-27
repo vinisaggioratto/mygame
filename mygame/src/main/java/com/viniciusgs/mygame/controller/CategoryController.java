5207c2aaef553634dcef23b32fbcb91bd8feb5e3
Vinicius G. Saggioratto <viniciussaggioratto@gmail.com>
2023-06-27 17:22:24 -0300

    Implementados novos métodos, alterar e deletar.

--- mygame/src/main/java/com/viniciusgs/mygame/controller/CategoryController.java
+++ mygame/src/main/java/com/viniciusgs/mygame/controller/CategoryController.java
@@ -3,13 +3,11 @@ package com.viniciusgs.mygame.controller;
 import com.viniciusgs.mygame.entity.Category;
 import com.viniciusgs.mygame.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping(value = "/category")
@@ -30,6 +28,28 @@ public class CategoryController {
     public ResponseEntity post(@RequestBody Category category) {
         try {
             return new ResponseEntity(categoryService.save(category), HttpStatus.CREATED);
+        } catch (Exception error) {
+            return new ResponseEntity(error.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping()
+    public ResponseEntity put(@RequestBody Category category) {
+
+        try {
+            return new ResponseEntity<>(categoryService.update(category), HttpStatus.OK);
+        } catch (Exception error) {
+            return new ResponseEntity(error.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity delete(@PathVariable long id) {
+        try {
+            categoryService.deleteById(id);
+            return new ResponseEntity<>("Categoria removida com sucesso", HttpStatus.OK);
+        } catch (EmptyResultDataAccessException error) {
+            return new ResponseEntity("Não foi possível localizar a categoria.", HttpStatus.BAD_REQUEST);
         } catch (Exception error) {
             return new ResponseEntity(error.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
--- mygame/src/main/java/com/viniciusgs/mygame/service/CategoryService.java
+++ mygame/src/main/java/com/viniciusgs/mygame/service/CategoryService.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryService {
@@ -26,5 +27,33 @@ public class CategoryService {
     public Category save(Category category){
         categoryRepository.save(category);
         return new Category();
+    }
+
+    //pesquisar as categorias por id
+    public Category findById(long id){
+        Optional<Category> optional = categoryRepository.findById(id);
+
+        if(!optional.isPresent()){
+            throw new RuntimeException("Category not found.");
+        }
+        Category category = optional.get();
+        return new Category();
+    }
+
+    //atualizar uma category
+    public Category update(Category category){
+        Optional<Category> userToEdit = categoryRepository.findById(category.getId());
+
+        if(!userToEdit.isPresent()){
+            throw new RuntimeException("Category not found.");
+        }
+        categoryRepository.save(category);
+        return new Category();
+    }
+
+    //delete a category
+    public void deleteById(long id){
+        categoryRepository.deleteById(id);
+
     }
 }
